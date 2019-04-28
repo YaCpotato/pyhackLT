f9d7c51113be6b46340548dba6e9f9ab7590d693
YaCpotato <yasshisshy@gmail.com>
2019-04-28 19:15:42 +0900

    create sample login

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 # coding: UTF-8
 
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/mypage',methods=['GET', 'POST'])
+def mypage():
+    if request.method == 'POST':
+		name = request.form['name']
+		password = request.form['password']
+		if(name == "admin" and password == "admin"):
+			return render_template('mypage.html',name=name, password=password)
+	
 
 def main():
 	app.debug = True
